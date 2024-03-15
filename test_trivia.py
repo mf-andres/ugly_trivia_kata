b34e060bf6410ee4aaa60e85c8a60839516bb887
mf-andres <amunoz@gradiant.org>
2024-03-15 10:51:02 +0100

    refactor: test when player answers correctly it gets one coin and refactor tests wih game fixture

--- test_trivia.py
+++ test_trivia.py
@@ -1,5 +1,15 @@
+import pytest
+
 from run_some_specific_games import run_games, save_outputs_to_file
 from trivia import Game
+
+
+@pytest.fixture
+def game():
+    game_ = Game()
+    game_.add_player("player1")
+    game_.add_player("player2")
+    return game_
 
 
 def test_trivia_works_as_before():
@@ -19,69 +29,51 @@ def text_files_are_equal(file1_path, file2_path):
     return content1 == content2
 
 
-def test_first_player_is_first_added_player():
-    game = Game()
-    game.add_player("player1")
-    game.add_player("player2")
+def test_first_player_is_first_added_player(game):
     assert game.current_player_object.name == "player1"
 
 
-def test_when_player1_answers_is_player2_turn():
-    game = Game()
-    game.add_player("player1")
-    game.add_player("player2")
+def test_when_player1_answers_is_player2_turn(game):
     game.handle_correct_answer()
     assert game.current_player_object.name == "player2"
 
 
-def test_when_player2_answers_is_player1_turn_again():
-    game = Game()
-    game.add_player("player1")
-    game.add_player("player2")
-    game.handle_correct_answer()  # player1
-    game.handle_correct_answer()  # player2
+def test_when_player2_answers_is_player1_turn_again(game):
+    game.handle_correct_answer()  # for player1
+    game.handle_correct_answer()  # for player2
     assert game.current_player_object.name == "player1"
 
 
-def test_when_gets_6_coins_it_wins_the_game():
-    game = Game()
-    game.add_player("player1")
-    game.add_player("player2")
+def test_when_gets_6_coins_it_wins_the_game(game):
     game.current_player_object.coins = 6
     assert game.current_player_object.has_won()
 
 
-def test_when_player_rolls_it_moves_places():
-    game = Game()
-    game.add_player("player1")
-    game.add_player("player2")
+def test_when_player_rolls_it_moves_places(game):
     game.handle_roll(1)
     assert game.current_player_object.place == 1
 
 
-def test_when_player_rolls_it_moves_around():
-    game = Game()
-    game.add_player("player1")
-    game.add_player("player2")
+def test_when_player_rolls_it_moves_around(game):
     game.current_player_object.place = 11
     game.handle_roll(1)
     assert game.current_player_object.place == 0
 
 
-def test_when_player_answers_wrongly_it_goes_to_penalty_box():
-    game = Game()
-    game.add_player("player1")
-    game.add_player("player2")
+def test_when_player_answers_wrongly_it_goes_to_penalty_box(game):
     game.handle_wrong_answer()
     first_player = game.players[0]
     assert first_player.is_in_penalty_box
 
 
-def test_when_player_in_penalty_box_rolls_odd_it_gets_out_of_the_penalty_box():
-    game = Game()
-    game.add_player("player1")
-    game.add_player("player2")
+def test_when_player_in_penalty_box_rolls_odd_it_gets_out_of_the_penalty_box(game):
     first_player = game.players[0]
     first_player.is_in_penalty_box = True
     game.handle_roll(3)
     assert first_player.is_getting_out_of_penalty_box
+
+
+def test_when_player_answers_correctly_it_gets_one_coin(game):
+    first_player = game.players[0]
+    game.handle_correct_answer()
+    assert first_player.coins == 1
